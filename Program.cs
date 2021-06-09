980b4f18fb1c8b6bdd8022c5ae1be4191313bf30
RAMZ-66 <axelramz_@hotmail.com>
2021-06-09 11:26:13 -0600

    Se desarrolló el programa, aunque presenta una falla debido a la variable de tiempo actual que da nombre al archivo .txt que se crea

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace P8_DiaryEntry
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            DateTime today = DateTime.Now;
+
+            CultureInfo culture = new CultureInfo("es-MX");
+
+            string todayAsStr = today.ToString(culture);
+
+            string nombreDocumento = todayAsStr.Substring(0,10);
+
+            string opcion = "";
+
+            while (opcion != "cerrar")
+            {
+
+                Console.WriteLine(todayAsStr);
+
+                Console.WriteLine("Porfavor introduce el texto por agregar a la entrada del diario, o introduce la palabra \"cerrar\" para dejar de escribir en el diario");
+
+                Console.WriteLine("");
+
+                string input = Console.ReadLine();
+
+                if (input == "cerrar")
+                {
+                    opcion = "cerrar";
+                    Console.WriteLine("\"Cerrando diario...\"");
+                }
+
+                // No se que pasa con la variable de tiempo "todayAsStr", si la utilizo para el nombre con el que se va a guardar 
+                // el atchivo .txt, me sale un error y el programa no se ejecuta correctamente, por lo que nombro el 
+                // archivo directamente con un string para que funcione
+                // No sé si estoy haciendo algo mal
+
+                else
+                {
+                    string DiaryEntry = input;
+
+                    using (StreamWriter streamWriter = new StreamWriter ($"{nombreDocumento}.txt"))
+                    {
+                        streamWriter.WriteLine(DiaryEntry);
+                    }
+                }
+
+
+            }
+
+
+
+        }
+    }
+}
